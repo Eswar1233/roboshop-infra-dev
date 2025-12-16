30faedaa38ad29da351473a2f415f2221c621660
Eswar <eswar.chandana@apoorva.com>
2025-12-16 18:11:57 +0530

    hello

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z1011474241AR8ZUHCM8Y"
+}
+
+variable "zone_name"{
+  default = "squareladdu.in"
+}
