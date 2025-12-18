a14705840159534fa3360ef2eaf491af23326ca2
Eswar <eswar.chandana@apoorva.com>
2025-12-18 07:00:21 +0530

    hello

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,4 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/Eswar1233/ansible-roboshop-roles.git -e component=$component  main.yaml
+ansible-pull -U https://github.com/Eswar1233/ansible-roboshop-roles-tf.git -e component=$component  -e env=$2 main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -34,7 +34,7 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mongodb"
+      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
     ]
   }
 }
@@ -81,7 +81,7 @@ resource "terraform_data" "redis" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh redis"
+      "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
     ]
   }
 }
@@ -126,7 +126,7 @@ resource "terraform_data" "mysql" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mysql"
+      "sudo sh /tmp/bootstrap.sh mysql ${var.environment}"
     ]
   }
 }
@@ -173,7 +173,7 @@ resource "terraform_data" "rabbitmq" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh rabbitmq"
+      "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
     ]
   }
 }
